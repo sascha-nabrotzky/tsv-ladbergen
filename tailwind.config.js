6eb558984a14f934dd70a1d0b5227f50c8280aa9
sascha-nabrotzky <sascha.nabrotzky@online.de>
2024-04-28 17:12:00 +0200

    remove tailwind - not working properly

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ['./packages/site_package/Resources/Private/**/*.html'],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
