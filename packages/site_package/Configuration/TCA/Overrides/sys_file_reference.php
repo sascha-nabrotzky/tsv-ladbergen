6704937d2878e1ba1439168f9b65d992c7d62e34
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2024-05-27 19:27:53 +0200

    add cropVariant to mainnav, add custm. prop., effects

--- packages/site_package/Configuration/TCA/Overrides/sys_file_reference.php
+++ packages/site_package/Configuration/TCA/Overrides/sys_file_reference.php
@@ -7,22 +7,32 @@ $field = 'crop';
 $GLOBALS['TCA'][$table]['columns'][$field]['config'] = [
      'type' => 'imageManipulation',
      'cropVariants' => [
-         'default' => [
-             'title' => 'Default',
+         'header' => [
+             'title' => 'Header',
              'allowedAspectRatios' => [
-                 '1:1' => [
-                     'title' => '1:1',
-                     'value' => 1 / 1
-                 ],
-                 '4:3' => [
-                     'title' => '4:3',
-                     'value' => 4 / 3
-                 ],
                  '16:9' => [
                      'title' => '16:9',
                      'value' => 16 / 9
                  ],
              ],
          ],
+         'menu' => [
+            'title' => 'Menu',
+            'allowedAspectRatios' => [
+                '1:1' => [
+                    'title' => '1:1',
+                    'value' => 1 / 1
+                ],
+            ],
+        ],
+        'article' => [
+            'title' => 'Article',
+            'allowedAspectRatios' => [
+                '4:3' => [
+                    'title' => '4:3',
+                    'value' => 4 / 3
+                ],
+            ],
+        ],
      ]
 ];
