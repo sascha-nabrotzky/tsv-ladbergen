ba33ba92757a36143085cb4761a7139d06aeb548
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2024-05-30 15:17:47 +0200

    fix: mobile btn animation

--- packages/site_package/Resources/Public/JavaScript/open-close-mobile-nav.js
+++ packages/site_package/Resources/Public/JavaScript/open-close-mobile-nav.js
@@ -1,9 +1,13 @@
 const mobileNav = document.querySelector?.('.mobile-nav-wrapper');
 const mobileNavButton = document.querySelector?.('.mobile-nav-button');
+const menuIconOpen = document.querySelector?.('.menu-icon-open');
+const menuIconClose = document.querySelector?.('.menu-icon-close');
 
 const toggleMobileNav = () => {
     mobileNavButton.addEventListener('click', () => {
         mobileNav.classList.toggle('open');
+        menuIconOpen.classList.toggle('hidden');
+        menuIconClose.classList.toggle('hidden');
     });
 };
 
