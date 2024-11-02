4ac1cb5cf9ae53df978786557b121593636541c4
sascha-nabrotzky <sascha.nabrotzky@online.de>
2024-11-02 17:54:55 +0100

    install german lang pack

--- config/system/settings.php
+++ config/system/settings.php
@@ -25,6 +25,13 @@ return [
             ],
         ],
     ],
+    'EXTCONF' => [
+        'lang' => [
+            'availableLanguages' => [
+                'de',
+            ],
+        ],
+    ],
     'EXTENSIONS' => [
         'backend' => [
             'backendFavicon' => '',
