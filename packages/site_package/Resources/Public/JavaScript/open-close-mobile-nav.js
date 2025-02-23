4c6910c1146010f227ceb2a57d07a556b25d3991
sascha-nabrotzky <sascha.nabrotzky@online.de>
2025-02-23 18:57:12 +0100

    toggle aria in mobile nav

--- packages/site_package/Resources/Public/JavaScript/open-close-mobile-nav.js
+++ packages/site_package/Resources/Public/JavaScript/open-close-mobile-nav.js
@@ -1,15 +1,46 @@
-const mobileNav = document.querySelector?.('.mobile-nav-wrapper');
-const mobileNavButton = document.querySelector?.('.mobile-nav-close-button');
+const mobileNavButton = document.querySelector?.('.mobile-nav-toggle-button');
 const menuIconOpen = document.querySelector?.('.menu-icon-open');
 const menuIconClose = document.querySelector?.('.menu-icon-close');
+const mobileNavWrapper = document.querySelector?.('.mobile-nav-wrapper');
+const mainContent = document.querySelector?.('.main-content-wrap');
 
-const toggleMobileNav = () => {
+const openMobileNav = () => {
     mobileNavButton.addEventListener('click', () => {
-        mobileNav.classList.toggle('open');
+        if (mobileNavButton.getAttribute('aria-expanded') === 'false') {
+            mobileNavButton.setAttribute('aria-expanded', 'true');
+        } else {
+            mobileNavButton.setAttribute('aria-expanded', 'false');
+        }
+
+        mobileNavWrapper.classList.toggle('open');
         menuIconOpen.classList.toggle('hidden');
         menuIconClose.classList.toggle('hidden');
-        console.log(getComputedStyle(menuIconClose).display);
+        mobileNavButton.toggleAttribute('aria-expanded', 'true');
+        mobileNavWrapper.toggleAttribute('inert');
     });
 };
+openMobileNav();
 
-toggleMobileNav();
+const setAriaHidden = () => {
+    if (document.documentElement.clientWidth < 640) {
+        mobileNavButton.setAttribute('aria-hidden', 'false');
+        mobileNavWrapper.setAttribute('aria-hidden', 'false');
+    } else {
+        mobileNavButton.setAttribute('aria-hidden', 'true');
+        mobileNavWrapper.setAttribute('aria-hidden', 'true');
+    }
+};
+setAriaHidden();
+
+const setAriaHiddenOnResize = () => {
+    window.addEventListener('resize', () => {
+        if (document.documentElement.clientWidth < 640) {
+            mobileNavButton.setAttribute('aria-hidden', 'false');
+            mobileNavWrapper.setAttribute('aria-hidden', 'false');
+        } else {
+            mobileNavButton.setAttribute('aria-hidden', 'true');
+            mobileNavWrapper.setAttribute('aria-hidden', 'true');
+        }
+    });
+};
+setAriaHiddenOnResize();
