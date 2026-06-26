18e79cc1e01e712efde09aa26d9fc5bb6d313c6e
sascha-nabrotzky <sascha.nabrotzky@online.de>
2026-06-26 15:34:59 +0200

    update typo3, fix img caption color, iframe height, primary btn alignment

--- packages/site_package/Resources/Public/JavaScript/set-iframe-height.js
+++ packages/site_package/Resources/Public/JavaScript/set-iframe-height.js
@@ -2,11 +2,11 @@ const iframe = document.querySelector?.('div:has(> iframe)');
 const openIframeButton = document.querySelector?.('.open-iframe-button');
 // Button muss manuell hinzugefügt werden (html-Element)
 const openIframeButtonContainer = document.querySelector?.(
-    'div:has(> .open-iframe-button)'
+    'div:has(> .open-iframe-button)',
 );
 
 function setHeight() {
-    iframe.style.height = '40rem';
+    iframe.style.height = '70rem';
     openIframeButton.removeEventListener('click', setHeight);
     openIframeButtonContainer.remove();
 }
